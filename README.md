a0914fc690cd68a57ada0ff8d93c2762fde5e44f
Akshara <akshblr555@gmail.com>
2021-09-18 11:12:54 +0530

    Added source

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# NER-IL
-NER for Indic languages
+# CL-NERIL
+Cross-Lingual Model for NER in Indian Languages
+
+Supplemental analaysis file is provided: Supplementary.pdf.
+
+Sample data files for 3 Indian languages are in /source/data/aligned. Full data (weakly labeled and gold standard) will be made public in future.
+
+To train the teacher model, download CoNLL English data and save in /source/data/conll/en.
+## To run
+```
+$ cd source
+$ ./scripts/run_clneril.sh
+```
+
+The results and logs will be stored in the output directory specified.
---
+++ source/run_clneril.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+GPU_IDS=0
+TGT_LANG=(hi bn gu)
+SRC_LANG=en
+seed=30
+
+for tgt_lang in ${TGT_LANG[@]}; do
+    echo $tgt_lang
+
+    # teacher training
+    python3 main_single.py --do_train --gpu_ids ${GPU_IDS} --seed ${seed} --learning_rate 5e-5 --data_dir ./data/conll/${SRC_LANG} --output_dir align-model-${seed}/mono-src-${SRC_LANG} --overwrite_cache
+
+    # train
+    python3 main.py --do_train --do_KD --gpu_ids ${GPU_IDS} --seed ${seed} --data_dir ./data/aligned/${tgt_lang} --src_langs ${SRC_LANG} --src_model_dir_prefix mono-src- --src_model_dir align-model-${seed} --output_dir joint-${seed}/ts-${SRC_LANG}-${tgt_lang}/ --overwrite_cache--joint_train
+
+    # test
+    python3 main.py --do_predict --gpu_ids ${GPU_IDS} --seed ${seed} --data_dir ./data/aligned/${tgt_lang} --output_dir joint-${seed}/ts-${SRC_LANG}-${tgt_lang}/ --overwrite_cache
+
+done
+
+
+
